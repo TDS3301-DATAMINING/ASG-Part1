6bd69329cbfaa29a18450437b604cf89a70d9412
yongching <yctee_04@outlook.com>
2016-12-30 02:02:22 +0800

    remove duplicate rows, select feature, imputation and convert to numeric

--- Preprocess.R
+++ Preprocess.R
@@ -1,8 +1,29 @@
+# read data
+setwd("")
 SP <- read.csv("Speed Dating Data.csv",na.strings = "")
 str(SP)
 
+# remove duplicate rows
+sp_unique <- sp[!duplicated(sp$iid), ]
+nrow(sp_unique)
+View(head(sp_unique))
 
+# feature selection
+sp_feature = sp_unique[,c("iid","age","gender","match","date","go_out","sports","tvsports","exercise","dining","museums",
+                   "art","hiking","gaming","clubbing","reading","tv","theater","movies","concerts","music",
+                   "shopping","yoga")]
+View(sp_feature)
+View(head(sp_feature))
+sum(is.na(sp_feature))
 
+# imputation using k-NN
+install.packages("VIM")
+library(VIM)
+sp_imputed <- kNN(sp_feature)
+sp_imputed <- sp_imputed[,names(sp_feature)]
+sum(is.na(sp_imputed))
 
-
-
+# convert to numeric
+sp_numeric <- sapply(sp_imputed, as.numeric)
+str(sp_numeric)
+is.numeric(sp_numeric)
